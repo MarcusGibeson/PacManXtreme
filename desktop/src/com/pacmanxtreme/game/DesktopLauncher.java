466f9411c4eaa393d4a0cc3a886ddceba68032fc
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2024-04-11 10:41:15 -0400

    Initial commit

---
+++ desktop/src/com/pacmanxtreme/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pacmanxtreme.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pacmanxtreme.game.PacManXtreme;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PacManXtreme");
+		new Lwjgl3Application(new PacManXtreme(), config);
+	}
+}
